51c0d8ac9c5bdba2d4b99d2916c3d72eac2c911e
Bahadir34 <bahadirahmetu@gmail.com>
2025-06-15 17:12:10 +0300

    updated submit event

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 //html den js eleman çekme dokument.querySelector ile getElemntByid,getElementByClass gibi özelliklerle de elemen çekilebiliyor.dokument.querySelector ile hem id hem de class çekilebiliyor. sadece id yada class çekmek isteseydik getElemntByid,getElemntByclass çzelliği ile çekmiş olacktık. class çağıdığımızda . ve id ile çağırdığımızda # eklememiz gerekiyor.
 
-const addBox=document.querySelector(".add-box");
-const popupContainer=document.querySelector(".popup-box");
-const popup=document.querySelector(".popup");
-const closeBtn=document.querySelector("#close-btn");
-const form=document.querySelector("form");
+const addBox = document.querySelector(".add-box");
+const popupContainer = document.querySelector(".popup-box");
+const popup = document.querySelector(".popup");
+const closeBtn = document.querySelector("#close-btn");
+const form = document.querySelector("form");
+
+console.log(form);
 
 //tıkalnam ,sürekleme, formül gönderme kısaca izleme olayını takip etmek için addEventListener() kullanılır.
-addBox.addEventListener("click",()=>{
-    //popup ı aktif etmek için :popupContainer ve popup elemanlarına class ı eklenmelidir. 
-    //class nasıl eklenir:popupContainer.classList.add(eklenecek clas)
-    popupContainer.classList.add("show");
-    popup.classList.add("show");
+addBox.addEventListener("click", () => {
+  //popup ı aktif etmek için :popupContainer ve popup elemanlarına class ı eklenmelidir.
+  //class nasıl eklenir:popupContainer.classList.add(eklenecek clas)
+  popupContainer.classList.add("show");
+  popup.classList.add("show");
 
-    //not ekrlerken arka planın sabit kalması için body alanına erşmemizgerekiyor.yani body nin kaydırımasını engelle
-    document.body.style.overflow="hidden";
-    
+  //not ekrlerken arka planın sabit kalması için body alanına erşmemizgerekiyor.yani body nin kaydırımasını engelle
+  document.body.style.overflow = "hidden";
 });
 //closeBtn ile eleman tıklandığını olay izleyicisi ekleyeyerek x dan çıkmay sağlayacak. bunun için de yukarıda eklediğimiz olay izleyicilerini remove ile eklenenşeri kaldırmışş olacağız
-closeBtn.addEventListener("click",()=>{
-   popupContainer.classList.remove("show");
-   popup.classList.remove("show");
+closeBtn.addEventListener("click", () => {
+  popupContainer.classList.remove("show");
+  popup.classList.remove("show");
 
-   //body nin tekrar aktif edilmesi için 
-   document.body.style.overflow="auto";
+  //body nin tekrar aktif edilmesi için
+  document.body.style.overflow = "auto";
 });
 
-form.addEventListener("sumbit",(e)=>{
-    //SAYFA YENİLNEMESİNİ ENGELLER
-    e.preventDefault();
-    console.log('form gönderildi');
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("merhaba");
 });
